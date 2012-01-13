4222b37d26956a7490de74376ce7994fdd6f52e5
Włodzimierz Gajda <gajdaw@gajdaw.pl>
2012-01-13 15:31:48 +0100

    Sprawdzenie ustalania galezi

--- 00-dodatki/tworzenie-pustej-bazy-danych/tworzenie-pustej-bazy-danych.sql
+++ 00-dodatki/tworzenie-pustej-bazy-danych/tworzenie-pustej-bazy-danych.sql
@@ -3,3 +3,4 @@ drop database if exists clipboard;
 create database clipboard default character set utf8 collate utf8_polish_ci;
 grant all on clipboard.* to clpadmin@localhost identified by 'sEcRetPassWoRD';
 flush privileges;
+
