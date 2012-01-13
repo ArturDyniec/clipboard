23772214256c415911110851f5902a84b2101408
Włodzimierz Gajda <gajdaw@gajdaw.pl>
2012-01-13 15:30:25 +0100

    Tworzenie pustej bazy

---
+++ 00-dodatki/tworzenie-pustej-bazy-danych/tworzenie-pustej-bazy-danych.sql
@@ -0,0 +1,5 @@
+set names utf8;
+drop database if exists clipboard;
+create database clipboard default character set utf8 collate utf8_polish_ci;
+grant all on clipboard.* to clpadmin@localhost identified by 'sEcRetPassWoRD';
+flush privileges;
