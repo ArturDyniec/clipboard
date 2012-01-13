58f5d4f2e27955f07785bc5c6332966782f1d93f
Karol Kozak <koralgol1@gmail.com>
2012-01-13 23:01:12 +0100

    Symfony 1.4.16

--- config/ProjectConfiguration.class.php
+++ config/ProjectConfiguration.class.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once 'C://xampp//php//PEAR//symfony/autoload/sfCoreAutoload.class.php';
+require_once dirname(__FILE__).'/../lib/vendor/symfony/lib/autoload/sfCoreAutoload.class.php';
 sfCoreAutoload::register();
 
 class ProjectConfiguration extends sfProjectConfiguration
