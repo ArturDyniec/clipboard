4221162197acadb074c309497b06b9080e4c5b64
gajdaw2 <wlodzimierz.gajda@gmail.com>
2012-01-26 17:44:02 +0100

    Zmiana folderu main: dodanie

---
+++ apps/frontend/modules/main/templates/showSuccess.php
@@ -0,0 +1,9 @@
+<h3>Tekst:</h3>
+<textarea id="source" rows="10" cols="3" name="source"></textarea>
+<input id="send" type="submit" value="Wyślij" name="submit">
+<br class="clearing" />
+<h3 class="nazwa">Nazwa:</h3>
+<input id="name" type="text" value="" name="name">
+<h3 class="nazwa">Autor:</h3>
+<input id="autor" type="text" value="" name="autor">    	
+<br class="clearing" />
