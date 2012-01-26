aa17970a9debd54e7d8c2d95954d30b603061d8c
Michal Z <bboykrim@gmail.com>
2012-01-26 16:26:54 +0100

    wygenerowane klasy

---
+++ lib/filter/doctrine/TekstFormFilter.class.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Tekst filter form.
+ *
+ * @package    clipboard
+ * @subpackage filter
+ * @author     Your name here
+ * @version    SVN: $Id: sfDoctrineFormFilterTemplate.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class TekstFormFilter extends BaseTekstFormFilter
+{
+  public function configure()
+  {
+  }
+}
---
+++ lib/form/doctrine/base/BaseTekstForm.class.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * Tekst form base class.
+ *
+ * @method Tekst getObject() Returns the current form's model object
+ *
+ * @package    clipboard
+ * @subpackage form
+ * @author     Your name here
+ * @version    SVN: $Id: sfDoctrineFormGeneratedTemplate.php 29553 2010-05-20 14:33:00Z Kris.Wallsmith $
+ */
+abstract class BaseTekstForm extends BaseFormDoctrine
+{
+  public function setup()
+  {
+    $this->setWidgets(array(
+      'id'         => new sfWidgetFormInputHidden(),
+      'contents'   => new sfWidgetFormTextarea(),
+      'created_at' => new sfWidgetFormDateTime(),
+      'updated_at' => new sfWidgetFormDateTime(),
+    ));
+
+    $this->setValidators(array(
+      'id'         => new sfValidatorChoice(array('choices' => array($this->getObject()->get('id')), 'empty_value' => $this->getObject()->get('id'), 'required' => false)),
+      'contents'   => new sfValidatorString(array('max_length' => 4096, 'required' => false)),
+      'created_at' => new sfValidatorDateTime(),
+      'updated_at' => new sfValidatorDateTime(),
+    ));
+
+    $this->widgetSchema->setNameFormat('tekst[%s]');
+
+    $this->errorSchema = new sfValidatorErrorSchema($this->validatorSchema);
+
+    $this->setupInheritance();
+
+    parent::setup();
+  }
+
+  public function getModelName()
+  {
+    return 'Tekst';
+  }
+
+}
